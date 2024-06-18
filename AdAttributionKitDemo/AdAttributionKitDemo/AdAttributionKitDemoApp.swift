4149098844ed3c1221e498bef347d1a84a9507de
arimura <33759663+arimura@users.noreply.github.com>
2024-06-18 13:22:40 +0900

    Add proj

---
+++ AdAttributionKitDemo/AdAttributionKitDemo/AdAttributionKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdAttributionKitDemoApp.swift
+//  AdAttributionKitDemo
+//
+//  Created by Kotaro Arimura on 2024/06/18.
+//
+
+import SwiftUI
+
+@main
+struct AdAttributionKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdAttributionKitDemo/AdAttributionKitDemoTests/AdAttributionKitDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdAttributionKitDemoTests.swift
+//  AdAttributionKitDemoTests
+//
+//  Created by Kotaro Arimura on 2024/06/18.
+//
+
+import Testing
+@testable import AdAttributionKitDemo
+
+struct AdAttributionKitDemoTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
